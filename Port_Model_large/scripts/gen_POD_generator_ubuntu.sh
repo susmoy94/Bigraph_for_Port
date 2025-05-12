a1de7b0f6229c0c4fac799d151eace948a413663
Susmoy Das <susmoy.das@glasgow.ac.uk>
2025-05-12 08:33:16 +0100

    All files are pushed

--- Port_Model_large/scripts/gen_POD_generator_ubuntu.sh
+++ Port_Model_large/scripts/gen_POD_generator_ubuntu.sh
@@ -3,5 +3,5 @@ directory="/Bigraph_for_Port/Port_Model_large/bigraph_models"
 
 for file in "$directory"/*.big; do
   BASENAME=$(basename "$file" .big)
-  bigrapher full $file -M 200000 -l ${BASENAME}.csl -p ${BASENAME}.tra --solver=GBS &
+  bigrapher full $file -M 100000 -l ${BASENAME}.csl -p ${BASENAME}.tra --solver=GBS &
 done
